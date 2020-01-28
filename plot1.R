170555c760e8cd19ab97ab21e417a9840c4da639
a.solis <solis.acevedo@gmail.com>
2020-01-28 15:26:26 +0100

    plot1

---
+++ plot1.R
@@ -0,0 +1,18 @@
+# reading data
+library(data.table)
+library(dplyr)
+library(chron)
+data <- fread("household_power_consumption.txt")
+finalData <- subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007")
+finalData <- mutate(finalData,Date=as.Date(Date, format = "%d/%m/%Y"))
+finalData <- mutate(finalData,Time=chron(times=finalData$Time))
+vars <- names(select(finalData,Global_active_power:Sub_metering_2))
+finalData[vars] <- sapply(finalData[vars],as.numeric)
+
+# creating plot1
+hist(finalData$Global_active_power, 
+     col = "red",main = "Global Active Power",xlab = "Global Active Power (kilowatts)")
+
+# creating PNG
+dev.copy(png, file = "plot1.png")
+dev.off()
